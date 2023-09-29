6222c7dae7e57a1376cfd54396672161df15e601
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-29 17:06:52 +0400

    Create MixinMinecraft.java

---
+++ src/main/java/com/example/examplemod/mixins/MixinMinecraft.java
@@ -0,0 +1,15 @@
+package com.example.examplemod.mixins;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tick(final CallbackInfo ci) {
+        System.out.println("Mixin worked!");
+    }
+}
